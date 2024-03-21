672fe9a2b6933b403b4e4d393b3c3055437d3d2b
Evghenii <geniann93@gmail.com>
2024-03-20 22:44:22 -0400

    Added TDBots

---
+++ BotsSource/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+zt-bcc TDBots.bcs ../ACS/TDBots.o
+m4 DECOM4.dec > ../DECORATE.tdb
+cd BotC
+./botc tdbot.botc ../../tdbot
+cd ../..
+cp tdbot crashbot.lump
+cp tdbot dfultbot.lump
+cp tdbot fatbot.lump
+cp tdbot humanbot.lump
+cp tdbot sausgbot.lump
